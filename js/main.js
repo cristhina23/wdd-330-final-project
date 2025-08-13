8e66d27b55f23ed5b47c801a155de27cb61abb37
Cristhina <viszellechacon@gmail.com>
2025-08-12 21:25:48 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import app from "./firebase.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 const auth = getAuth(app);
 
@@ -7,19 +7,15 @@ const userPhotoImg = document.getElementById("userPhoto");
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    
     if (user.photoURL) {
-      
       userPhotoImg.src = user.photoURL;
       userPhotoImg.alt = user.displayName || "User profile photo";
       userPhotoImg.style.borderRadius = "50%"; 
     } else {
-      
       userPhotoImg.src = "ruta/a/tu/icono-usuario.svg"; 
       userPhotoImg.alt = "User icon";
     }
   } else {
-   
     userPhotoImg.src = "ruta/a/tu/icono-default.svg";
     userPhotoImg.alt = "Default icon";
   }
